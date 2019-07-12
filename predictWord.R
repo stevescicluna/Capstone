e0246edd2477bbebdcd28a0ed7440f51e5cf4aec
Steve Scicluna <46481869+stevescicluna@users.noreply.github.com>
2019-07-12 21:33:11 +1000

    Add files via upload

--- predictWord.R
+++ predictWord.R
@@ -8,10 +8,10 @@
 Predict <- function(text) { 
         if (nchar(text) == 0) 
                 return("")
-
+        
         predWord <- ""
-
-# Take the last three words of the search term
+        
+        # Take the last three words of the search term
         
         truncateText3 <- function(text, wordCount = 3) {
                 truncText3 <- paste(
@@ -19,14 +19,14 @@ Predict <- function(text) {
                 return(truncText3)
         }
         
-# Take the last two words of the search term 
+        # Take the last two words of the search term 
         
         truncateText2 <- function(text, wordCount = 2) {
                 truncText2 <- paste(tail(unlist(strsplit(text, " ")), wordCount), collapse = " ")
                 return(truncText2)
         }
         
-# Take the last word of the search term
+        # Take the last word of the search term
         
         truncateText1 <- function(text, wordCount = 1) {
                 truncText1 <- paste(tail(unlist(strsplit(text, " ")), wordCount), collapse = " ")
@@ -34,35 +34,19 @@ Predict <- function(text) {
         }
         
         
-# For prediction of the next word, quadgram data is first used.
-# The first three words of the quadgram are the last three words entered by the user.
+        # For prediction of the next word, quadgram data is first used.
+        # The first three words of the quadgram are the last three words entered by the user.
         
         predWord <- head(quadgrams[grep(truncateText3(text), quadgrams[,1]),],3)$fourth
-         
-        if (nchar(predWord) > 0) {
-                return(predWord)
-                } else {
         
-# If no matching quadgram is found, back off to trigram data.
-# The first two words of the trigram are the last two words entered by the user.
+        ifelse((nchar(predWord)>0), return(predWord), 
+               predWord <- head(trigrams[grep(truncateText2(text), trigrams[,1]),],3)$third)
         
-        predWord <- head(trigrams[grep(truncateText2(text), trigrams[,1]),],3)$third
-                
-        if (nchar(predWord) > 0) {
-                return(predWord)
-                } else {
-
-# If no matching trigram is found, back off to bigram data.
-# The first word of the bigram is the last word entered by the user.
+        ifelse((nchar(predWord)>0), return(predWord),
+               predWord <- head(bigrams[grep(truncateText1(text), bigrams[,1]),],3)$second)
         
-        predWord <- head(bigrams[grep(truncateText1(text), bigrams[,1]),],3)$second
-                
-        if (nchar(predWord) > 0) {
-                return(predWord)
-                } else {
-                        predWord <- "N/A"
-                        predWord
-                }
-                }
-                }
+        ifelse((nchar(predWord)>0), return(predWord),
+               predWord <- "N/A")
+        
+        return(predWord)
 }
